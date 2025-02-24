db7c664162edcdafc30676fad65d8d6c0ed44c05
Dracula-flow <avaglianosimone@gmail.com>
2025-02-24 14:18:18 +0100

    New version 1.5.5
    Added the possibility to paste screenshots automatically in order of creation for the Sanity testing procedures. 
    
    Added two functions for file format checking, making code more readable.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+
+# Author: Simone Avagliano
+# Date Created: 25/02/2025
+# Description: Pyzelius v.1.5.1, a dashboard to help manual testing
+# License: Creative Commons Zero v1.0 Universal
+
 # Simple launcher for the app
 
 from app.Gui import Gui
--- src/Functions.py
+++ src/Functions.py
@@ -1,7 +1,10 @@
 from datetime import datetime as dt
 
-# A function to return different time components in the form of a formatted string, based on the input parameter
+
 def time_responser(selector):
+    """
+    A function to return different time components in the form of a formatted string, based on the input parameter
+    """
     date = dt.now().strftime('%d-%m-%Y')
     time = dt.now().strftime('%H:%M')
 
@@ -17,6 +20,15 @@ def time_responser(selector):
         if selector not in output:
             raise ValueError(f"Error: {selector} is NOT a valid parameter. Time_responser only takes 'date', 'time' and 'datetime' as parameters")
 
-# A function to check for file format
+
 def is_test(filename):
+    """
+    Checks for video file format
+    """
     return filename.lower().endswith(('.mp4','.mov')) 
+
+def is_screenshot(filename):
+    """
+    Checks for image file format
+    """
+    return filename.lower().endswith(('.jpg','.png'))
